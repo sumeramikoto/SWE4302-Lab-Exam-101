e9dad9fff60ea9ad87c190c6d7c97a8683e19b13
Navid Ibrahim <nibrahim7787@gmail.com>
2024-11-20 15:16:50 +0600

    Added Ice Cream Topping class

---
+++ src/main/java/IceCreamTopping.java
@@ -0,0 +1,25 @@
+public class IceCreamTopping {
+    private String name;
+    private double cost;
+
+    public IceCreamTopping(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
